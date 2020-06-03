ebe0a94c257a53642659be7487ca61e6a2bc4baa
yasirshahid <66242488+yasirshahid@users.noreply.github.com>
2020-06-03 15:22:07 +0500

    updated score template

---
+++ Score.js
@@ -0,0 +1,24 @@
+var score=prompt("what is your score");
+score=parseInt(score);
+
+if (score==80 || score>80){
+    alert("Grade A+")
+    if (score==85 || score>85){
+        alert("REWARD $100")
+    }
+    else{
+        alert("reward $20")
+    }
+}
+else if(score==70 || score>70){
+    alert("Grade A");
+}
+else if (score==60 || score>60){
+    alert("Grade B")
+}
+else if (score==50 || score>50){
+    alert("Grade C")
+}
+else {
+    alert("failed");
+}
